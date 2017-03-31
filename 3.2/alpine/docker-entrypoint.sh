f0a9daab56685913e298276de9d5d7676e2791e9
Rubén Norte <ruben.norte@softonic.com>
2017-03-31 10:13:16 +0200

    Modified docker-entrypoint to convert environment variables with form REDIS_CONFIG_XXX into CLI arguments

--- 3.2/alpine/docker-entrypoint.sh
+++ 3.2/alpine/docker-entrypoint.sh
@@ -1,6 +1,19 @@
 #!/bin/sh
 set -e
 
+# convert REDIS_CONFIG environment variables into CLI arguments
+# e.g.: REDIS_CONFIG_MAXMEMORY_POLICY='allkeys-lru' => --maxmemory-policy 'allkeys-lru'
+get_arguments_from_environment() {
+	args=""
+	for env_var in $(printenv | cut -f1 -d"=" | grep -e "^REDIS_CONFIG_")
+	do
+		arg_name=$(echo ${env_var#REDIS_CONFIG_} | tr "[:upper:]" "[:lower:]" | sed -e "s/_/-/g")
+		arg_value=$(printenv $env_var)
+		args="$args --$arg_name '$arg_value'"
+	done
+	echo $args
+}
+
 # first arg is `-f` or `--some-option`
 # or first arg is `something.conf`
 if [ "${1#-}" != "$1" ] || [ "${1%.conf}" != "$1" ]; then
@@ -11,6 +24,12 @@ fi
 if [ "$1" = 'redis-server' -a "$(id -u)" = '0' ]; then
 	chown -R redis .
 	exec su-exec redis "$0" "$@"
+fi
+
+args=$(get_arguments_from_environment)
+
+if [ "$args" != "" ] && [ "$1" = "redis-server" ]; then
+	set -- "$@" "$args"
 fi
 
 exec "$@"
